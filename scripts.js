f209badc69fd267b0df3345a7059ae3ed31254a2
winstonaviation <winstonshek2005@gmail.com>
2024-02-24 23:59:26 -0500

    a

--- scripts.js
+++ scripts.js
@@ -1,14 +1,37 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 var Amadeus = require("amadeus");
 var amadeus = new Amadeus({
   clientId: 'anp2n0Uce9eAvtLm58Y4igIGsKnhyTTH',
   clientSecret: '2eyrH6qJsAkMqoD5'
 });
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/airportSearch/', function(req,res,next){ 
+    amadeus.referenceData.locations.get({ 
+      keyword: req.query.term, 
+      subType: 'AIRPORT,CITY' 
+    }).then(function(response){ 
+      res.json(response.data); 
+      console.log(response.data.iataCode); 
+    }).catch(function(error){ 
+      console.log("error"); 
+      console.log(error.response); 
+    }); 
+    }); 
 // What relevant airports are there around a specific location?
-amadeus.referenceData.locations.airports.get({
-  longitude: 2.55,
-  latitude: 49.0000
-}).then(function (response) {
-  console.log(response);
-}).catch(function (response) {
-  console.error(response);
-});
+/*
+amadeus.referenceData.locations.hotels.byCity.get({
+   cityCode: 'PAR'
+  }).then(function (response) {
+    console.log(response);
+  }).catch(function (response) {
+    console.error(response);
+  });
+*/
